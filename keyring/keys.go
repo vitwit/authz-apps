fbcd2d4432082a72594f8ff4354214bee7333519
aleem1314 <aleem@vitwit.com>
2023-05-23 21:19:57 +0530

    fix: fix mutex lock

--- keyring/keys.go
+++ keyring/keys.go
@@ -1,4 +1,4 @@
-package keyshandler
+package keyring
 
 import (
 	"context"
--- voting/voting.go
+++ voting/voting.go
@@ -171,7 +171,7 @@ func (v *Vote) ExecVote(chainName, pID, granter, vote, fromKey, metadata, memo, 
 		return "", fmt.Errorf("failed to vote.Err: %v", err)
 	}
 	if err := v.Db.AddLog(chainInfo.ChainID, pID, vote); err != nil {
-		fmt.Errorf("failed to store logs: %v", err)
+		fmt.Printf("failed to store logs: %v", err)
 	}
 	mintscanName := chainName
 	if newName, ok := regisrtyNameToMintscanName[chainName]; ok {
