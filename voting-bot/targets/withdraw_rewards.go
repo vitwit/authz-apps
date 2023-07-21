940a0daea2377ac12b7486b099d90714342ea2b3
0xmuralik <krishna192reddy@gmail.com>
2023-07-21 14:23:25 +0530

    withdraw rewards

---
+++ voting-bot/targets/withdraw_rewards.go
@@ -0,0 +1,285 @@
+package targets
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/slack-go/slack"
+	lensclient "github.com/strangelove-ventures/lens/client"
+	registry "github.com/strangelove-ventures/lens/client/chain_registry"
+	"github.com/vitwit/authz-apps/voting-bot/types"
+	"github.com/vitwit/authz-apps/voting-bot/voting"
+	"go.uber.org/zap"
+
+	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	distribution "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	"github.com/vitwit/authz-apps/voting-bot/endpoints"
+)
+
+func Withdraw(ctx types.Context) error {
+	keys, err := ctx.Database().GetKeys()
+	if err != nil {
+		return err
+	}
+	validators, err := ctx.Database().GetValidators()
+	if err != nil {
+		return err
+	}
+
+	for _, key := range keys {
+		validEndpoint, err := endpoints.GetValidEndpointForChain(key.ChainName)
+		if err != nil {
+			log.Printf("Error in getting valid LCD endpoints for %s chain", key.ChainName)
+			return err
+		}
+
+		chainInfo, chainClient, err := createChainClient(ctx, key.ChainName, key.KeyName)
+		if err != nil {
+			log.Printf("Error in creating chain client for %s chain", key.ChainName)
+			return err
+		}
+
+		for _, val := range validators {
+			if val.ChainName == key.ChainName {
+				var msgs []*cdctypes.Any
+
+				accAddr, err := convertValAddrToAccAddr(ctx, val.Address, key.ChainName)
+				if err != nil {
+					return err
+				}
+
+				ops := types.HTTPOptions{
+					Endpoint: validEndpoint + "/cosmos/authz/v1beta1/grants?granter=" + accAddr + "&grantee=" + key.KeyAddress + "&msg_url_type=/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
+					Method:   http.MethodGet,
+				}
+				g1, err := getAuthzGrants(ops.Endpoint)
+				if err != nil {
+					return err
+				}
+				if len(g1) > 0 {
+					msg, err := withdrawRewardsMsg(accAddr, val.Address)
+					if err != nil {
+						log.Printf("Error in creating withdraw rewards message for %s", val.Address)
+						return err
+					}
+
+					msgs = append(msgs, msg)
+				}
+
+				ops = types.HTTPOptions{
+					Endpoint: validEndpoint + "/cosmos/authz/v1beta1/grants?granter=" + accAddr + "&grantee=" + key.KeyAddress + "&msg_url_type=/cosmos.distribution.v1beta1.MsgWithdrawValidatorCommission",
+					Method:   http.MethodGet,
+				}
+				g2, err := getAuthzGrants(ops.Endpoint)
+				if err != nil {
+					return err
+				}
+
+				if len(g2) > 0 {
+					msg, err := withdrawCommissionMsg(val.Address)
+					if err != nil {
+						log.Printf("Error in creating withdraw commission message for %s", val.Address)
+						return err
+					}
+
+					msgs = append(msgs, msg)
+				}
+
+				res, err := executeMsgs(chainClient, msgs, key.KeyAddress)
+				if err != nil {
+					log.Printf("Error in creating withdraw commission message for %s", val.Address)
+					return err
+				}
+
+				mintscanName := val.ChainName
+				if newName, ok := voting.RegisrtyNameToMintscanName[val.ChainName]; ok {
+					mintscanName = newName
+				}
+
+				rewards, err := getRewardAmount(res, "withdraw_rewards")
+				if err != nil {
+					log.Printf("Error in getting rewards from tx resp for chain %s. txhash: %s", key.ChainName, res.TxHash)
+					return err
+				}
+
+				commission, err := getRewardAmount(res, "withdraw_commission")
+				if err != nil {
+					log.Printf("Error in getting rewards from tx resp for chain %s. txhash: %s", key.ChainName, res.TxHash)
+					return err
+				}
+
+				url := fmt.Sprintf("https://mintscan.io/%s/txs/%s", mintscanName, res.TxHash)
+				SendMsgExecAlert(ctx, val.Address, url)
+
+				denom, err := voting.GetChainDenom(chainInfo)
+				if err != nil {
+					log.Printf("Error in getting denom for chain %s", val.ChainName)
+					return err
+				}
+
+				if err := ctx.Database().AddRewards(chainInfo.ChainID, denom, val.Address, rewards.String(), commission.String()); err != nil {
+					log.Printf("Failed to store reward and commission for %s on %s", val.Address, val.ChainName)
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func createChainClient(ctx types.Context, chainName, keyName string) (registry.ChainInfo, lensclient.ChainClient, error) {
+	// Fetch chain info from chain registry
+	chainInfo, err := voting.GetChainInfo(ctx, chainName)
+	if err != nil {
+		return registry.ChainInfo{}, lensclient.ChainClient{}, fmt.Errorf("chain info not found: %v", err)
+	}
+
+	//	Use Chain info to select random endpoint
+	rpc, err := chainInfo.GetRandomRPCEndpoint(ctx.Context())
+	if err != nil {
+		return registry.ChainInfo{}, lensclient.ChainClient{}, fmt.Errorf("failed to get random RPC endpoint on chain %s. Err: %v", chainInfo.ChainID, err)
+	}
+	denom, err := voting.GetChainDenom(chainInfo)
+	if err != nil {
+		return registry.ChainInfo{}, lensclient.ChainClient{}, fmt.Errorf("failed to get denom from chain %s: %v", chainInfo.ChainID, err)
+	}
+
+	gasPrices := "0.25" + denom
+
+	chainConfig := lensclient.ChainClientConfig{
+		Key:            keyName,
+		ChainID:        chainInfo.ChainID,
+		RPCAddr:        rpc,
+		AccountPrefix:  chainInfo.Bech32Prefix,
+		KeyringBackend: "test",
+		GasPrices:      gasPrices,
+		Debug:          true,
+		Timeout:        "20s",
+		GasAdjustment:  1.4,
+		OutputFormat:   "json",
+		SignModeStr:    "direct",
+		Modules:        lensclient.ModuleBasics,
+	}
+
+	curDir, err := os.Getwd()
+	if err != nil {
+		return registry.ChainInfo{}, lensclient.ChainClient{}, fmt.Errorf("error while getting current directory: %v", err)
+	}
+
+	// Create client object to pull chain info
+	chainClient, err := lensclient.NewChainClient(zap.L(), &chainConfig, curDir, os.Stdin, os.Stdout)
+	if err != nil {
+		return registry.ChainInfo{}, lensclient.ChainClient{}, fmt.Errorf("failed to build new chain client for %s. Err: %v", chainInfo.ChainID, err)
+	}
+	return chainInfo, *chainClient, nil
+}
+
+func executeMsgs(chainClient lensclient.ChainClient, msgs []*cdctypes.Any, keyAddr string) (*sdk.TxResponse, error) {
+	req := &authz.MsgExec{
+		Grantee: keyAddr,
+		Msgs:    msgs,
+	}
+
+	// Send msg and get response
+	res, err := chainClient.SendMsg(context.Background(), req, "")
+	if err != nil {
+		if res != nil {
+			return nil, fmt.Errorf("failed to vote on proposal: code(%d) msg(%s)", res.Code, res.Logs)
+		}
+		return nil, fmt.Errorf("failed to vote.Err: %v", err)
+	}
+
+	return res, nil
+}
+
+// Generate withdraw rewards message
+func withdrawRewardsMsg(granter, valAddr string) (*cdctypes.Any, error) {
+	var msgAny *cdctypes.Any
+
+	msgWithdrawRewards := distribution.MsgWithdrawDelegatorReward{
+		DelegatorAddress: granter,
+		ValidatorAddress: valAddr,
+	}
+
+	msgAny, err := cdctypes.NewAnyWithValue(&msgWithdrawRewards)
+	if err != nil {
+		return nil, fmt.Errorf("error on converting msg to Any: %v", err)
+	}
+
+	return msgAny, nil
+}
+
+// Generate withdraw commission message
+func withdrawCommissionMsg(valAddr string) (*cdctypes.Any, error) {
+	var msgAny *cdctypes.Any
+
+	msgWithdrawCommission := distribution.MsgWithdrawValidatorCommission{
+		ValidatorAddress: valAddr,
+	}
+
+	msgAny, err := cdctypes.NewAnyWithValue(&msgWithdrawCommission)
+	if err != nil {
+		return nil, fmt.Errorf("error on converting msg to Any: %v", err)
+	}
+
+	return msgAny, nil
+}
+
+// SendMsgExecAlert which sends alerts on withdraw rewards and commission txs
+func SendMsgExecAlert(ctx types.Context, valAddr, URL string) error {
+	api := ctx.Slacker().APIClient()
+
+	attachment := []slack.Block{
+		slack.NewHeaderBlock(
+			slack.NewTextBlockObject("plain_text", fmt.Sprintf("Withdraw rewards and commission executed for %s\nTransaction broadcasted: %s", valAddr, URL), false, false),
+		),
+	}
+
+	_, _, err := api.PostMessage(
+		ctx.Config().Slack.ChannelID,
+		slack.MsgOptionBlocks(attachment...),
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getRewardAmount(res *sdk.TxResponse, eventType string) (sdk.Coins, error) {
+	var totalRewards sdk.Coins
+
+	for _, event := range res.Events {
+		if event.Type == eventType {
+			for _, attr := range event.Attributes {
+				var attribute types.EventAttribute
+				bz, err := json.Marshal(attr)
+				if err != nil {
+					return nil, fmt.Errorf("error marshalling event attribute: %v", err)
+				}
+
+				if err := json.Unmarshal(bz, &attribute); err != nil {
+					return nil, fmt.Errorf("error unmarshalling event attribute: %v", err)
+				}
+
+				if attribute.Key == "amount" {
+					rewards, err := sdk.ParseCoinsNormalized(attribute.Value)
+					if err != nil {
+						return nil, fmt.Errorf("error parsing reward coins: %v", err)
+					}
+					for _, reward := range rewards {
+						totalRewards.Add(reward)
+					}
+				}
+			}
+		}
+	}
+
+	return totalRewards, nil
+}
