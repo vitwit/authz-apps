217af0325a1010fd82409804f91ebe3717672d83
0xmuralik <krishna192reddy@gmail.com>
2023-07-21 14:23:10 +0530

    rewards table

--- voting-bot/database/sqlite.go
+++ voting-bot/database/sqlite.go
@@ -32,6 +32,15 @@ type (
 		VoteOption    string
 	}
 
+	rewardsCommission struct {
+		ChainID    string
+		Denom      string
+		ValAddr    string
+		Rewards    string
+		Commission string
+		Date       string
+	}
+
 	Sqlitedb struct {
 		db *sql.DB
 	}
@@ -68,6 +77,11 @@ func (a *Sqlitedb) InitializeTables() error {
 	}
 
 	_, err = a.db.Exec("CREATE TABLE IF NOT EXISTS keys (chainName VARCHAR PRIMARY KEY, keyName VARCHAR, keyAddress VARCHAR)")
+	if err != nil {
+		return err
+	}
+
+	_, err = a.db.Exec("CREATE TABLE IF NOT EXISTS rewards_commission (chainId VARCHAR, denom VARCHAR, valAddress VARCHAR, rewards VARCHAR, commission VARCHAR, date VARCHAR)")
 	if err != nil {
 		return err
 	}
@@ -137,6 +151,18 @@ func (a *Sqlitedb) AddLog(chainName, proposalTitle, proposalID, voteOption strin
 	defer stmt.Close()
 
 	_, err = stmt.Exec(time.Now().UTC().Unix(), chainName, proposalTitle, proposalID, voteOption)
+	return err
+}
+
+func (a *Sqlitedb) AddRewards(chainId, denom, valAddr, rewards, commission string) error {
+	stmt, err := a.db.Prepare("INSERT INTO rewards_commission(chainId, denom, valAddress, rewards, commission, date) values(?,?,?,?,?)")
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(chainId, denom, valAddr, rewards, commission, time.Now().Format("2006-01-02"))
 	return err
 }
 
