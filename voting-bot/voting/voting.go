ebaf488519462adbcc892e4170fc5714b2332bb5
aleem1314 <aleem@vitwit.com>
2023-09-03 22:36:20 +0530

    chore: uncomment

--- voting-bot/voting/voting.go
+++ voting-bot/voting/voting.go
@@ -142,7 +142,7 @@ func ExecVote(ctx types.Context, chainName, pID, granter, vote,
 		Msgs:    []*cdctypes.Any{msgAny},
 	}
 
-	// responseWriter.Reply(fmt.Sprintf("voting %s on %s proposal %d", voteOption, chainName, proposalID))
+	responseWriter.Reply(fmt.Sprintf("voting %s on %s proposal %d", voteOption, chainName, proposalID))
 	// Send msg and get response
 	res, err := chainClient.SendMsg(context.Background(), req, memo)
 	if err != nil {
